c8bfd9e6a05fc864733c39ad3a01c6f3cd138f2b
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-01-13 18:44:16 +0530

    fitness micro service

--- userservice/src/main/java/com/fitness/userservice/models/User.java
+++ userservice/src/main/java/com/fitness/userservice/models/User.java
@@ -1,7 +1,21 @@
 package com.fitness.userservice.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "users")
+@Data
 public class User {
+    private String id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private UserRole role = UserRole.USER;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
