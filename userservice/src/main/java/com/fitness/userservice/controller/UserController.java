3cdd4238b57778734a99b3f780c463652cd64a52
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-01-13 22:10:47 +0530

    fitness micro service

---
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.fitness.userservice.controller;
+
+
+import com.fitness.userservice.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@AllArgsConstructor
+public class UserController {
+    private UserService userService;
+}
