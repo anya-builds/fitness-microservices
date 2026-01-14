349e0105e192948ca2b81119550458eac78f1deb
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-01-14 15:12:04 +0530

    fitness micro service

--- userservice/src/main/java/com/fitness/userservice/controller/UserController.java
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.fitness.userservice.controller;
 
 
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
 import com.fitness.userservice.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +18,8 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponse> register(@RequestBody RegisterRequesr request){
-        return ResponsEntity.ok(userService.register(request));
+    public ResponseEntity<UserResponse> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(userService.register(request));
 
     }
 
