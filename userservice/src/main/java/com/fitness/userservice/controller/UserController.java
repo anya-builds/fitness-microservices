1ec505e28579366fb26d35037f4fdd63339ddac8
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-01-14 15:01:32 +0530

    fitness micro service

--- userservice/src/main/java/com/fitness/userservice/controller/UserController.java
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -1,24 +1,24 @@
-//package com.fitness.userservice.controller;
-//
-//
-//import com.fitness.userservice.service.UserService;
-//import lombok.AllArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/api/users")
-//@AllArgsConstructor
-//public class UserController {
-//    private UserService userService;
-//
-//    @PostMapping("/register")
-//    public ResponseEntity<UserResponse> register(@RequestBody RegisterRequesr request){
-//        return ResponsEntity.ok(userService.register(request));
-//
-//    }
-//
-//}
+package com.fitness.userservice.controller;
+
+
+import com.fitness.userservice.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@AllArgsConstructor
+public class UserController {
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@RequestBody RegisterRequesr request){
+        return ResponsEntity.ok(userService.register(request));
+
+    }
+
+}
